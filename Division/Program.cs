9ad7cd216c6bd720f80b622a93421ba1060f852f
churikov-sv <churikov.sv@outlook.com>
2023-09-19 23:07:07 +0300

    add Random

--- Division/Program.cs
+++ Division/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿double nubmerA = 5;
+double nubmerB = 10;
+double div = nubmerA / nubmerB;
+Console.WriteLine(div);
--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int nubmerA = 3;
-int nubmerB = 7;
+﻿int nubmerA = new Random().Next(1, 10);
+Console.WriteLine(nubmerA);
+int nubmerB = new Random().Next(1, 10);
+Console.WriteLine(nubmerB);
 int sum = nubmerA + nubmerB;
 Console.WriteLine(sum);
