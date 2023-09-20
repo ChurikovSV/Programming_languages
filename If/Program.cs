46ea0f245da28923c7645e67e063f4732d2aed5e
churikov-sv <churikov.sv@outlook.com>
2023-09-20 23:08:05 +0300

    add If

---
+++ If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "мури")
+{
+    Console.WriteLine("Ура, это Мури");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
