1642d5f3b7b1aacbacf8eeb40cbc8ae0f050d530
churikov-sv <churikov.sv@outlook.com>
2023-09-25 23:11:48 +0300

    add IntroArray

---
+++ IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {9, 10, 25};
+
+int n = array.Length;
+int find = 10;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
