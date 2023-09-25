b43ed4b0cfb500802944c6afec43a12cb06cf2e1
churikov-sv <churikov.sv@outlook.com>
2023-09-25 23:56:58 +0300

    add Task

---
+++ Task/Program.cs
@@ -0,0 +1,27 @@
+﻿int MaxDigit(int num)
+{
+    int fDigit = num / 10;
+    int sDigit = num % 10;
+    return fDigit > sDigit ? fDigit : sDigit;
+} 
+
+int number = new Random().Next(10, 99 + 1);
+// int fDigit = number / 10;
+// int sDigit = number % 10;
+
+// if(fDigit > sDigit)
+// {
+//     Console.WriteLine($"Наибольшее число {fDigit}");
+
+// }
+// else {
+//     Console.WriteLine($"Наибольшее число {sDigit}");
+// }
+
+// int maxDigit = fDigit > sDigit ? fDigit : sDigit;
+
+// Console.WriteLine($"Наибольшее {maxDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшее {maxDigit}");
+
