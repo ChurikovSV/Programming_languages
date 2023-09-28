ba19e8b1396f79b7c22bfeb424308f1941d8c3c5
churikov-sv <churikov.sv@outlook.com>
2023-09-28 23:01:08 +0300

    add Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,17 @@
+﻿int fNumber = Convert.ToInt32(Console.ReadLine());
+int sNumber = Convert.ToInt32(Console.ReadLine());
+
+int MulNumber (int num1, int num2)
+{
+    return num1 % num2;
+}
+int mul = MulNumber(fNumber, sNumber);
+
+if (mul == 0)
+{
+    Console.WriteLine($"кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно");
+}
