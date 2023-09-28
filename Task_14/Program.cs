698532644e0b43384fef4942bf3adc82f63cf8a4
churikov-sv <churikov.sv@outlook.com>
2023-09-28 23:16:06 +0300

    add Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,20 @@
+﻿int fNumber = 7;
+int sNumber = 23;
+int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % fNumber == 0 && number % sNumber == 0)
+// {
+//     Console.WriteLine($"да");
+// }
+// else
+//     Console.WriteLine($"нет");
+
+
+bool MulNumber(int num1, int num2, int num3)
+{
+    return num1 % num2 == 0 && num1 % num3 == 0;
+}
+
+bool result = MulNumber(number, fNumber, sNumber);
+
+Console.WriteLine(result ? $"да" : $"Нет");
