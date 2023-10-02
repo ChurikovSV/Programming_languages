4bb519d35bb0b81ace3583422c192c748bdaf599
churikov-sv <churikov.sv@outlook.com>
2023-10-02 23:40:20 +0300

    add For

---
+++ For/Program.cs
@@ -0,0 +1,65 @@
+﻿// for (int i = 2; i < 10; i++)
+// {
+//     for( int j = 0; j < 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, ";
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// string newText2 = Replace(text, 'к', 'К');
+// Console.WriteLine(newText2);
+
+
+int[] arr = {1, 5, 4, 3};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPos = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPos]) minPos = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
--- Task/Program.cs
+++ Task/Program.cs
@@ -6,21 +6,6 @@
 } 
 
 int number = new Random().Next(10, 99 + 1);
-// int fDigit = number / 10;
-// int sDigit = number % 10;
-
-// if(fDigit > sDigit)
-// {
-//     Console.WriteLine($"Наибольшее число {fDigit}");
-
-// }
-// else {
-//     Console.WriteLine($"Наибольшее число {sDigit}");
-// }
-
-// int maxDigit = fDigit > sDigit ? fDigit : sDigit;
-
-// Console.WriteLine($"Наибольшее {maxDigit}");
 
 int maxDigit = MaxDigit(number);
 Console.WriteLine($"Наибольшее {maxDigit}");
